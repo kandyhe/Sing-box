8e01c6d723cf8d2f445dabbe14ff6ecc06442540
kooooooook <178030060@qq.com>
2024-08-08 14:42:26 +0800

    Update sb_serv00.sh

--- sb_serv00.sh
+++ sb_serv00.sh
@@ -14,7 +14,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b9'}
+export UUID=${UUID:-'a3ae7a7d-271b-4fd3-8ae2-06b94fb6cec0'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
